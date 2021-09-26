275acdeefc4e59f292046c62b6fdb9f49ea07c89
guaguawa0 <510446679@qq.com>
2021-09-26 16:26:55 +0800

    实现单例服务器

--- echo/server/source/main.c
+++ echo/server/source/main.c
@@ -7,40 +7,50 @@
 
 static const char* EXEC_DIR_PATH = "/dev/myEcho/";
 static const char* EXEC_FILE_PATH = "/dev/myEcho/echo.if";
-static const int g_InfoFd = 0;
+static int g_InfoFd = 0;
+static int g_uniqueLock = 0;
 
-enum BOOL {
+typedef enum _BOOL {
     TRUE = 0,
     FALSE,
-};
+} BOOL;
 
-inline void PrintHelp()
+static inline void PrintHelp()
 {
     printf("Please Use \"Server start\" or \"Server stop\"\n");
 }
 
 /* 该程序第一次执行 */
-inline BOOL CreateInfoFile()
+static inline BOOL CreateInfoFile()
+{
+    int ret = 0;
+    if(access(EXEC_DIR_PATH, F_OK) != 0) {      
+        mkdir(EXEC_DIR_PATH, 0777);      
+    }
+    g_InfoFd = open(EXEC_FILE_PATH, O_RDWR | O_CREAT);
+    if (g_InfoFd == -1) {
+        return FALSE;
+    }
+    ret = write(g_InfoFd, "kai shi le", 10);
+    return TRUE;
+}
+
+/* 通过文件锁检查是不是独一份的服务器，如果是的话会清空记录文件 */
+static inline BOOL IsUniqueServer()
 {
     g_InfoFd = open(EXEC_FILE_PATH, O_RDWR | O_CREAT);
     if (g_InfoFd == -1) {
         return FALSE;
     }
-    return TRUE;
-}
-
-/* 通过文件锁检查是不是独一份的服务器，如果是的话会清空记录文件 */
-inline BOOL IsUniqueServer()
-{
-    if (access(EXEC_FILE_PATH, 0) == 0) {
-
+    if (flock(g_InfoFd, LOCK_EX | LOCK_NB) != 0) {
+        return FALSE;
     }
     return TRUE;
 }
 
-inline BOOL CheckRunEnv()
+static inline BOOL CheckRunEnv()
 {
-    if (access(EXEC_FILE_PATH, 0) == 0) {
+    if (access(EXEC_FILE_PATH, 0) != 0) {
         return CreateInfoFile();
     } else {
         return IsUniqueServer();
@@ -49,14 +59,16 @@ inline BOOL CheckRunEnv()
 
 int main(int args, char* argv[])
 {
-    if (args != 3) {
+    if (args != 2) {
         PrintHelp();
         return 0;
     }
     if (CheckRunEnv() == FALSE) {
-        printf("run error");
+        printf("run error\n");
         return 0;
     }
-    printf("run success");
+    for (;;) {
+    }
+    printf("run success\n");
     return 0;
 }
